3a3b959442fffb3748d64fb548970907b622b820
NickPonikiewski <nponikie@UMICH.EDU>
2017-03-11 16:34:48 -0500

    Base

---
+++ CIS427Client/CIS427Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace CIS427Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = Encoding.ASCII.GetBytes("Hi");
+            string IP = "127.0.0.1";
+            int port  = 904;
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(IP), port);
+
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            client.SendTo(data, endpoint);
+
+            while(true)
+            {
+
+            }
+
+        }
+    }
+}
